0819e24cd7bdf1595d59443f7506039082d8abe6
issacstorm <XFZgithub!@#123>
2020-05-08 21:52:33 -0400

    init file

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='levenpandas',         # How you named your package folder (MyLib)
     packages=['levenpandas'],   # Chose the same as "name"
-    version='0.1.1',      # Start with a small number and increase it with every change you make
+    version='0.1.2',      # Start with a small number and increase it with every change you make
     # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     license='MIT',
     # Give a short description about your library
